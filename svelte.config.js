626c4660ca032cd6bcbcf02e321e72e2a9cc82d4
Giacomo <giacomo.folli@studenti.unipr.it>
2025-02-09 14:12:08 +0100

    temp

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 				sizes: [640, 828, 1200, 1920, 3840],
 				formats: ['image/avif', 'image/webp'],
 				minimumCacheTTL: 300,
-				domains: ['bernini.vercel.app']
+				domains: []
 			}
 		})
 		// inlineStyleThreshold: 5000
