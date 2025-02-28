2bc98cb38f33b8e2748ac934e336fcf70b7ecea4
Giacomo <giacomo.folli@studenti.unipr.it>
2025-02-28 17:59:43 +0100

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
 	import Footer from '$lib/components/global/Footer.svelte';
 	import Navigation from '$lib/components/global/Navigation.svelte';
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { onMount } from 'svelte';
+
 	let { children } = $props();
+	onMount(() => injectAnalytics());
 </script>
 
 <Navigation />
